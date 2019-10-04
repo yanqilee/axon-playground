008a17c9dc47f7f0823cb491e07cadd0b17517e7
Yanqi Li <yanqilee@hotmail.com>
2019-10-04 13:46:51 -0400

    Cleaned up REST APIs

--- src/main/java/com/example/demo/controller/GiftCardController.java
+++ src/main/java/com/example/demo/controller/GiftCardController.java
@@ -4,6 +4,8 @@ import com.example.demo.command.IssueCmd;
 import com.example.demo.command.RedeemCmd;
 import com.example.demo.model.CardSummary;
 import com.example.demo.service.GiftCardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,20 +21,21 @@ public class GiftCardController {
         this.redeemService = redeemService;
     }
 
-    @RequestMapping("/issue")
-    public String issue() {
-        redeemService.issue(new IssueCmd("gc1", 100));
-        redeemService.issue(new IssueCmd("gc2", 50));
+    @GetMapping("/issue/{amount}")
+    public IssueCmd issue(@PathVariable(value = "amount") Integer amount) {
+        IdGenerator.increment();
+        IssueCmd issueCmd = new IssueCmd(Integer.toString(IdGenerator.getId()), amount);
+        redeemService.issue(issueCmd);
 
-        return "Success";
+        return issueCmd;
     }
 
-    @RequestMapping("/redeem")
-    public String redeem() {
-        redeemService.redeem(new RedeemCmd("gc1", 10));
-        redeemService.redeem(new RedeemCmd("gc2", 20));
+    @RequestMapping("/redeem/{id}/{amount}")
+    public RedeemCmd redeem(@PathVariable(value = "id") Integer id, @PathVariable(value = "amount") Integer amount) {
+        RedeemCmd redeemCmd = new RedeemCmd(Integer.toString(id), amount);
+        redeemService.redeem(redeemCmd);
 
-        return "Success";
+        return redeemCmd;
     }
 
     @RequestMapping("/summary")
